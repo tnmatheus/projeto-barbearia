8003fa24f32fd750b5c766190baf25387fbb8687
tnmatheus <mth7768@gmail.com>
2024-06-05 20:54:39 -0300

    Crianção dos elementos HTML

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,13 +1,32 @@
 export default function LandingPage() {
   return (
-    <header>
-      <div>
-        <img src="assets/barbearia-logo.png" alt="logomarca" />
-        <button>
-          <img src="assets/moon.png" alt="icone" />
-          Dark
-        </button>
-      </div>
-    </header>
+    <div>
+      <header>
+        <div>
+          <img src="assets/barbearia-logo.png" alt="logomarca" />
+          <button>
+            <img src="assets/moon.png" alt="icone" />
+            Dark
+          </button>
+        </div>
+      </header>
+      <section></section>
+      <section>
+        <div>
+          <h1>Bem-vindo a Barber Shop</h1>
+          <p>
+            Nossa barbearia sempre oferece profissionais de qualidade e estamos
+            prontos para lidar com suas maiores expectativas.
+          </p>
+          <p>
+            Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma
+            equipe premiada que demonstrou o talento de mestres barbeiros em
+            vários concursos de estilo. Deixe nosso barbeiro ser seu estilista
+            pessoal e você nunca ficará desapontado.
+          </p>
+          <p>S. Kelly</p>
+        </div>
+      </section>
+    </div>
   );
 }
