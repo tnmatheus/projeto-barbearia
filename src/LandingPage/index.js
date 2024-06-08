b49657e54f1a275006bf0bfae82843de613a6ebd
tnmatheus <mth7768@gmail.com>
2024-06-07 21:19:05 -0300

    Ajustes esteticos na pagina

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,30 +1,38 @@
+import "./style.css";
+
 export default function LandingPage() {
   return (
-    <div className="container">
-      <header className="limitar-secao">
-        <div>
-          <img src="assets/barbearia-logo.png" alt="logomarca" />
-          <button className="btn">
-            <img src="assets/moon.png" alt="icone" />
-            Dark
+    <div className="modo-escuro">
+      <header>
+        <div className="limitar-secao">
+          <img
+            className="logomarca"
+            src="assets/barbearia-logo.png"
+            alt="logomarca"
+          />
+          <button className="modo-escuro">
+            <img className="btn-icone" src="assets/sun.png" alt="icone" />
+            Light
           </button>
         </div>
       </header>
-      <section className="secao-img"></section>
-      <section className="secao-textos">
-        <div>
+
+      <section className="secao-banner"></section>
+
+      <section className="secao-texto limitar-secao">
+        <div className="limitar-secao">
           <h1>Bem-vindo a Barber Shop</h1>
-          <p>
+          <p className="primeiro-paragrafo">
             Nossa barbearia sempre oferece profissionais de qualidade e estamos
             prontos para lidar com suas maiores expectativas.
           </p>
-          <p>
+          <p className="segundo-paragrafo">
             Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma
             equipe premiada que demonstrou o talento de mestres barbeiros em
             vários concursos de estilo. Deixe nosso barbeiro ser seu estilista
             pessoal e você nunca ficará desapontado.
           </p>
-          <p>S. Kelly</p>
+          <p className="assinatura">S. Kelly</p>
         </div>
       </section>
     </div>
